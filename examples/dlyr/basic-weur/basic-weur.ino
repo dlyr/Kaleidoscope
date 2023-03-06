8bafc63ba0d7096654255e56709fce316dbad8db
dlyr <github@dlyr.fr>
2023-03-06 10:48:37 +0100

    compiles

--- examples/dlyr/basic-weur/basic-weur.ino
+++ examples/dlyr/basic-weur/basic-weur.ino
@@ -195,9 +195,8 @@ enum {
 
 enum {
   PRIMARY,
-  FUNCTION,
-  NUMNAV,
   SYMBOLS,
+  NUMNAV,
   EUR
 };  // layers
 
@@ -235,7 +234,7 @@ KEYMAPS(
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(NUMNAV),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___            ,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
@@ -252,7 +251,7 @@ KEYMAPS(
    Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
    Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(NUMNAV),
 
 #elif defined (PRIMARY_KEYMAP_COLEMAK)
 
@@ -262,14 +261,14 @@ KEYMAPS(
    Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_D, Key_V, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(NUMNAV),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___              ,
    Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
                   Key_M, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
    Key_RightAlt,  Key_K, Key_H, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   ShiftToLayer(NUMNAV)),
 
 #elif defined (PRIMARY_KEYMAP_CUSTOM)
   // Edit this keymap to make a custom layout
@@ -279,55 +278,53 @@ KEYMAPS(
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(NUMNAV),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___              ,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   ShiftToLayer(NUMNAV)),
 #else
 
 #error "No default keymap defined. You should make sure that you have a line like '#define PRIMARY_KEYMAP_QWERTY' in your sketch"
 
 #endif
 
-  [NUMNAV] = KEYMAP_STACKED
-  (___,            Key_F1,  Key_F2,      Key_F3,      Key_F4,        Key_F5,           Key_CapsLock,
-  Key_Tab,         Key_1,   Key_2,       Key_3,       Key_4,         Key_5,             ___,
-  ___,             ___,     Key_Home,    Key_PageUp,  Key_End,       ___,
-  Key_PrintScreen, ___,      Key_Ins,    Key_PageDown, ___,          ___,               ___,
-  ___,             Key_Delete, ___,      ___,
-  ___,
-
-  Consumer_ScanPreviousTrack, Key_F6, Key_F7,        Key_F8,      Key_F9,         Key_F10, Key_F11,
-  Consumer_ScanNextTrack,     Key_6,  Key_7,         Key_8,       Key_9,          Key_0,  Key_F12,
-                               ___,   Key_LeftArrow, Key_UpArrow, Key_RightArrow, Key_Period, Key_Comma,
-  Key_Menu,                   Consumer_Mute,   Consumer_VolumeDecrement, Key_DownArrow, Consumer_VolumeIncrement, Consumer_PlaySlashPause, Consumer_Stop,
-  ___, ___, Space, ___,
-  ___),
 [SYMBOLS] =  KEYMAP_STACKED
-(//
-        ___                  , Key_ExclamationMark        , Key_LessThan    , Key_Equal            , Key_GreaterThanSHIFT   ,        Key_Minus     , ___                  ,
-        ___                  , Key_BackSlash        , Key_LeftParen        , Key_Dollar        , Key_RightParen       , Key_Plus             , ___                  ,
-        ___                  , Key_Asterisk        , Key_At        , Key_Colon                  , Key_Slash                  ,Key_Pound                  , // MIDDLE LARGER
-   ___                  , ___                  , ___                  , ___                  , ___                  , ___                  , ___                  ,
-   // LEFT THUMB
-   ___                  , ___                  , ___                  , ___                  ,
-   // LEFT PALM
-   ___                  ,
-// END FIRST HALF
-   ___                  , ___                  , ___                  , ___                  , ___                  , ___                  , ___                  ,
-   ___                  , Key_Carat            , Key_LeftBracket      , Key_Underscore       , Key_RightBracket     , Key_DoubleQuote      , ___                  ,
- /* MIDDLE LARGER */    , Key_Percent          , Key_LeftCurlyBracket , Key_SemiColon        , Key_RightCurlyBracket, Key_Quote            , ___                  ,
-   ___                  , Key_BackTick         , Key_Pipe             , Key_Tilde            , Key_Ampersand        , Key_QuestionMark     , ___                  ,
-   // RIGHT THUMB
-   ___                  , ___                  , ___                  , ___                  ,
-   // RIGHT PALM
+(
+   ___, Key_ExclamationPoint, Key_LessThan, Key_Equals, Key_GreaterThan, Key_Minus, ___,
+   ___, Key_Backslash, Key_LeftParen, Key_Dollar, Key_RightParen, Key_Plus, ___,
+   ___, Key_Asterisk, Key_At, Key_Colon, Key_Slash, Key_Pound,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, Key_Carat, Key_LeftBracket, Key_Underscore, Key_RightBracket, Key_DoubleQuote, ___,
+        Key_Percent, Key_LeftCurlyBracket, Key_Semicolon, Key_RightCurlyBracket, Key_Quote, ___,
+   ___, Key_Backtick, Key_Pipe, Key_Tilde, Key_Ampersand, Key_QuestionMark, ___,
+   ___, ___, ___, ___,
    ___
-// END KBD
-)
+    ),
+
+  [NUMNAV] =  KEYMAP_STACKED
+  (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
+   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   ___, Key_Delete, ___, ___,
+   ___,
+
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   ___, ___, Key_Enter, ___,
+   ___)
+
+
 ) // KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
